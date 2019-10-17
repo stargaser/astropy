0384683224a1f4f0325f5dc1d503b9cc9c7bca4f
David Shupe <shupe@ipac.caltech.edu>
2019-10-17 10:41:46 +0700

    add ScienceState tests in new session

--- astropy/constants/tests/test_sciencestate.py
+++ astropy/constants/tests/test_sciencestate.py
@@ -1,4 +1,7 @@
 import pytest
+import subprocess
+import sys
+import tempfile
 
 from astropy import physical_constants, astronomical_constants
 import astropy.constants as const
@@ -19,3 +22,28 @@ def test_previously_imported():
 
     with pytest.raises(RuntimeError):
         astronomical_constants.set('iau2015')
+
+
+def test_new_session():
+    teststr = """
+import astropy
+astropy.physical_constants.set('codata2014')
+import astropy.constants as const
+import astropy.units as u
+assert abs((const.M_sun / u.M_sun).to(u.dimensionless_unscaled) - 1) < 1.e-14
+assert const.M_sun.reference[-11:] == 'CODATA 2014'
+    """
+
+    with tempfile.NamedTemporaryFile() as fd:
+        fd.write(teststr.encode())
+        fd.seek(0)
+        result = subprocess.run([sys.executable, fd.name])
+        assert result.returncode == 0
+
+    newstr = teststr.replace('CODATA 2014', 'CODATA 2012')
+
+    with tempfile.NamedTemporaryFile() as fd:
+        fd.write(newstr.encode())
+        fd.seek(0)
+        result = subprocess.run([sys.executable, fd.name])
+        assert result.returncode == 1
